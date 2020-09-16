9a98da1f3335915603ccd00f9b49533c779bf702
Eber-Oliveira <eber.oliv.costa@gmail.com>
2020-09-16 17:28:38 -0300

    POST records

---
+++ backend/src/main/java/com/eberoliveira/dspesquisa/services/RecordService.java
@@ -0,0 +1,40 @@
+package com.eberoliveira.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.eberoliveira.dspesquisa.dto.RecordDTO;
+import com.eberoliveira.dspesquisa.dto.RecordInsertDTO;
+import com.eberoliveira.dspesquisa.entities.Game;
+import com.eberoliveira.dspesquisa.entities.Record;
+import com.eberoliveira.dspesquisa.repositories.GameRepository;
+import com.eberoliveira.dspesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+	
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		repository.save(entity);
+		return new RecordDTO(entity);
+		
+	}
+}
