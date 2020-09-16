d63d2926e5c98af70b00f4c79740798d2443987e
Eber-Oliveira <eber.oliv.costa@gmail.com>
2020-09-16 16:24:41 -0300

    DTO, Service

---
+++ backend/src/main/java/com/eberoliveira/dspesquisa/dto/GameDTO.java
@@ -0,0 +1,50 @@
+package com.eberoliveira.dspesquisa.dto;
+
+import java.io.Serializable;
+
+import com.eberoliveira.dspesquisa.entities.Game;
+import com.eberoliveira.dspesquisa.entities.enums.Platform;
+
+public class GameDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String title;
+	private Platform platform;
+	
+	public GameDTO() {
+		
+	}
+	
+	public GameDTO(Game entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		platform = entity.getPlatform();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Platform getPlatfomr() {
+		return platform;
+	}
+
+	public void setPlatfomr(Platform platfomr) {
+		this.platform = platfomr;
+	}
+	
+
+}
